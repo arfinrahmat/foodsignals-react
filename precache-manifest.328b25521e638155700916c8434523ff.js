206d41b859789512411de810d1f7fb622afdf88f
Arfin Rahmat <arfinrahmat13@gmail.com>
2020-06-23 17:54:14 +0700

    deploy

---
+++ precache-manifest.328b25521e638155700916c8434523ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c93a8de828c4ad7428591bc434d45e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53985ef0aa17a6617e5f",
+    "url": "/static/css/main.3c406871.chunk.css"
+  },
+  {
+    "revision": "305da2ee1a133e4501ac",
+    "url": "/static/js/2.929a51a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.929a51a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53985ef0aa17a6617e5f",
+    "url": "/static/js/main.c0ae04cf.chunk.js"
+  },
+  {
+    "revision": "b82dc7eed05d4c2b81fe",
+    "url": "/static/js/runtime-main.c63e177e.js"
+  },
+  {
+    "revision": "de1c5244f981787e45cb3a549bb23c09",
+    "url": "/static/media/aneka-makanan.de1c5244.jpg"
+  }
+]);
